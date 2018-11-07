ddd47b26f601b453d1a2b155952624639ee455f4
gimjiu <evillion99@gmail.com>
2018-11-08 00:58:02 +0900

    카잉달력

---
+++ 181108-6064.py
@@ -0,0 +1,35 @@
+import math
+T = int(input())
+
+for _ in range(T):
+    M, N, x, y = list(map(int, input().split(' ')))
+    ans = x
+    ax = x
+    if M > N:
+        ay = x % N
+        if ay == 0:
+            ay += N
+        ri = M-N
+
+    else:
+        ay = x
+        ri = M
+
+    width = (M*N) // math.gcd(M, N) // M
+
+    while width >= 0:
+        if ax == x and ay == y:
+            break
+        ay += ri
+        if ay > N:
+            ay = ay % N
+            if ay == 0:
+                ay += N
+        ans += M
+
+        width -= 1
+
+    if ax == x and ay == y:
+        print(ans)
+    else:
+        print(-1)
