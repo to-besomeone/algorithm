4c1e0a04f47247d18e186ce37c77615ddca40a04
gimjiu <evillion99@gmail.com>
2018-11-08 19:03:46 +0900

    0nC0der 1번 수정

--- 181108-oncoder1.py
+++ 181108-oncoder1.py
@@ -1,13 +1,8 @@
 left, right = list(map(int, input().split(' ')))
-mat = [0 for _ in range(right-left+1)]
-string = '36'
 cnt = 0
+
 for i in range(left, right+1):
-    mat[i-left] = str(i)
-
-
-for i in mat:
-    if string in i:
+    if '36' in str(i):
         cnt += 1
 
 print(cnt)
