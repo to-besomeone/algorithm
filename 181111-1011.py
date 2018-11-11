d9e81fcef89f757054ce75511f74f6ed8e22f249
gimjiu <evillion99@gmail.com>
2018-11-11 20:37:04 +0900

    Fly me to the Alpha Centauri (코드 리뷰 무조건 필요)

---
+++ 181111-1011.py
@@ -0,0 +1,19 @@
+T = int(input())
+
+
+for _ in range(T):
+    x, y = list(map(int, input().split()))
+    distance = y-x
+    a = 0
+    while True:
+        maxdis = 0
+        a += 1
+        if a%2 == 0:
+            maxdis = a//2*(a//2+1)
+        elif a%2 == 1:
+            maxdis = a//2*(a//2+1) + a//2 + 1
+        if distance <= maxdis:
+            print(a)
+            break
+
+
